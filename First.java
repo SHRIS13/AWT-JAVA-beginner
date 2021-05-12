357a5af79bbc574bfb476d11fd3feb51edfb2eb0
Shristi Sisodiya <505shristi@gmail.com>
2021-05-12 22:39:41 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,27 @@
+package com.swingShristi;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class First {
+    private JButton button_msg;
+    private JPanel panelMain;
+
+    public First() {
+        button_msg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Hello Swing");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("First Swing GUI");
+        frame.setContentPane(new First().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
