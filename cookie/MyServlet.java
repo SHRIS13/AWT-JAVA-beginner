f77d2dca7b3c6ef0c833476f20147522e91d2122
Shristi Sisodiya <505shristi@gmail.com>
2021-06-05 04:04:19 +0530

    Add files via upload

---
+++ cookie/MyServlet.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/MyServlet"})
+public class MyServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+
+            
+            if(password.equals("1234"))
+        {
+            Cookie ck = new Cookie("username", username);
+            response.addCookie(ck);
+            response.sendRedirect("First");
+        }
+            else
+            {
+                out.println("Sorry, Your user-login Failed ");
+            }
+        }
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
