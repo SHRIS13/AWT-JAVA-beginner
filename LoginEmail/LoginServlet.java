ee719c124e9b1a5c387c44a9ac298e6554f39927
Shristi Sisodiya <505shristi@gmail.com>
2021-06-07 12:20:49 +0530

    Add files via upload

---
+++ LoginEmail/LoginServlet.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            String mail = request.getParameter("mail");
+            String password = request.getParameter("password");
+
+            if(mail.equals("505shristi@gmail.com") && password.equals("1234"))
+            {
+                out.println("Welcome "+mail+" Email login Successful!!");
+            }
+            else
+            {
+                out.println("Sorry, Your user-login Failed ");
+            }
+        }
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
